68e5bb25b05f9aee5b906065519c3819dfb042a3
washington786 <dkmawasha@gmail.com>
2022-11-28 02:16:12 +0200

    commit

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,45 +1,52 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ScreenWrapper from "../components/ScreenWrapper/ScreenWrapper";
 import { useNavigation } from "@react-navigation/native";
-import { Avatar, Button, Snackbar } from "react-native-paper";
+import {
+  ActivityIndicator,
+  Avatar,
+  Button,
+  Dialog,
+  Paragraph,
+  Portal,
+  Provider,
+  Snackbar,
+} from "react-native-paper";
 import InputWrapper from "../components/InputWrapper/InputWrapper";
 import GlobalInput from "../components/TextInput/GlobalInput";
 import GlobalButton from "../components/GlobalButton/GlobalButton";
 import { GlobalColors } from "../infrastructure/GlobalColors";
-import { db,auth } from "../Firebase";
+import { db, auth } from "../Firebase";
 const SettingScreen = () => {
-  const [name,setName]=useState('')
-  const [email,setEmail]=useState('')
-  const [Email,setemail]=useState('')
-  const [phonenumber,setPhonenumber]=useState('')
-  const [Fullname,setFullname]=useState('')
-  const [Phonenumber,setphonenumber]=useState('')
-  const [uid,setUid]=useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [Email, setemail] = useState("");
+  const [phonenumber, setPhonenumber] = useState("");
+  const [Fullname, setFullname] = useState("");
+  const [Phonenumber, setphonenumber] = useState("");
+  const [uid, setUid] = useState("");
   const user = auth.currentUser.uid;
-  useEffect(()=>{
-      db.ref(`/Bususers/`+ user).on('value',snap=>{
-        
-        setName(snap.val() && snap.val().name);
-    setEmail(snap.val().email)
-    setPhonenumber(snap.val().phonenumber)
- 
-  setUid(snap.val().uid)
-      })
-    
-      
-    },[])
-    const editprofile=()=>{
-   
-  db.ref('Bususers').child(user).update({name:Fullname,email:Email,phonenumber:Phonenumber})
-      .then(()=>db.ref('Bususers').once('value'))
-      .then(snapshot=>snapshot.val())
-      .catch(error => ({
+  useEffect(() => {
+    db.ref(`/Bususers/` + user).on("value", (snap) => {
+      setName(snap.val() && snap.val().name);
+      setEmail(snap.val().email);
+      setPhonenumber(snap.val().phonenumber);
+
+      setUid(snap.val().uid);
+    });
+  }, []);
+  const editprofile = () => {
+    db.ref("Bususers")
+      .child(user)
+      .update({ name: Fullname, email: Email, phonenumber: Phonenumber })
+      .then(() => db.ref("Bususers").once("value"))
+      .then((snapshot) => snapshot.val())
+      .catch((error) => ({
         errorCode: error.code,
-        errorMessage: error.message
+        errorMessage: error.message,
       }));
-      // setIsSnackBarVisible(!isSnackBarVisible);
-    }
+    // setIsSnackBarVisible(!isSnackBarVisible);
+  };
   const navigation = useNavigation();
 
   const [isSnackBarVisible, setIsSnackBarVisible] = useState(false);
@@ -48,6 +55,34 @@ const SettingScreen = () => {
     setIsSnackBarVisible(!isSnackBarVisible);
   };
 
+  const [dialogVisible, setDialogVisible] = useState(false);
+
+  const [isLoading,setIsLoading] = useState(false);
+
+  if(isLoading){
+    return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <ActivityIndicator size={'large'} color={GlobalColors.green}/>
+      <Paragraph>Signing off...</Paragraph>
+    </View>
+  }
+
+  const onShow=()=>{
+    setDialogVisible(true);
+  }
+  const onDismiss=()=>{
+    setDialogVisible(false);
+  }
+
+  const onLogoutUser=()=>{
+    setIsLoading(true)
+    setDialogVisible(false);
+    auth.signOut();
+    navigation.navigate('login');
+    setIsLoading(false);
+    setDialogVisible(false);
+  }
+
+
   return (
     <ScreenWrapper>
       <ScrollView
@@ -55,8 +90,8 @@ const SettingScreen = () => {
         contentContainerStyle={{
           alignItems: "center",
           justifyContent: "flex-start",
-          flex:1,
-          marginTop:60
+          flex: 1,
+          marginTop: 60,
         }}
       >
         <View style={styles.avatar}>
@@ -67,9 +102,9 @@ const SettingScreen = () => {
           <GlobalInput
             config={{
               placeholder: "Karabo Daniel",
-            
-              onChangeText:(name)=>setFullname(name),
-              value:name,
+
+              onChangeText: (name) => setFullname(name),
+              value: name,
             }}
             customStyle={styles.input}
             icon="account"
@@ -77,7 +112,7 @@ const SettingScreen = () => {
           <GlobalInput
             config={{
               placeholder: "Mawasha",
-              value:uid,
+              value: uid,
             }}
             customStyle={styles.input}
             icon="account"
@@ -85,8 +120,8 @@ const SettingScreen = () => {
           <GlobalInput
             config={{
               placeholder: "dkmawasha@gmail.com",
-              onChangeText:(email)=>setemail(email),
-              value:email,
+              onChangeText: (email) => setemail(email),
+              value: email,
             }}
             customStyle={styles.input}
             icon="email"
@@ -94,8 +129,8 @@ const SettingScreen = () => {
           <GlobalInput
             config={{
               placeholder: "+27817263718",
-              onChangeText:(phonenumber)=>setphonenumber(phonenumber),
-              value:phonenumber,
+              onChangeText: (phonenumber) => setphonenumber(phonenumber),
+              value: phonenumber,
             }}
             customStyle={styles.input}
             icon="phone"
@@ -110,6 +145,35 @@ const SettingScreen = () => {
           >
             UPDATE
           </Button>
+
+          <Button
+            style={styles.button}
+            mode="outlined"
+            color={GlobalColors.bg}
+            onPress={onShow}
+            icon={"exit-to-app"}
+          >
+            Logout
+          </Button>
+
+          <Provider>
+            <View>
+              <Portal>
+                <Dialog visible={dialogVisible} onDismiss={onDismiss}>
+                  <Dialog.Title>Logout Account</Dialog.Title>
+                  <Dialog.Content>
+                    <Paragraph>
+                      Are you sure you want to logout your account?
+                    </Paragraph>
+                  </Dialog.Content>
+                  <Dialog.Actions>
+                    <Button onPress={onDismiss}>Cancel</Button>
+                    <Button onPress={onLogoutUser}>Logout</Button>
+                  </Dialog.Actions>
+                </Dialog>
+              </Portal>
+            </View>
+          </Provider>
         </InputWrapper>
 
         <Snackbar
